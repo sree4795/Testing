f71f24aacf5d67a0029e414e9417f8941a50b329
srinivasareddy4218 <63068380+srinivasareddy4218@users.noreply.github.com>
2022-07-22 19:15:58 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,6 +1,4 @@
-Invoke-AzVMRunCommand -ResourceGroupName 'test' -VMName 'test' -CommandId 'RunPowerShellScript' -ScriptString 
-'Install-WindowsFeature -Name Web-Server -IncludeManagementTools'
-#New-Item -Path 'C:\Test Folder' -ItemType Directory
+New-Item -Path 'C:\Test Folder' -ItemType Directory
 #Set-TimeZone -Name "Pacific Standard Time"
 #Git pull https://github.com/sree4795/loginapp.git
 
