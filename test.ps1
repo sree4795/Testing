a593e01b192976c8018a0d2714ee539c730d4804
srinivasareddy4218 <63068380+srinivasareddy4218@users.noreply.github.com>
2022-07-22 16:17:29 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-New-Item -Path 'D:\temp\Test Folder' -ItemType Directory
+New-Item -Path 'C:\Test Folder' -ItemType Directory
 Set-TimeZone -Name "Pacific Standard Time"
 Git pull https://github.com/sree4795/loginapp.git
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
